042a8f48f45d5d4d9e9416aae0a9216e66936ad3
gmu01 <gmu01@Chan>
2020-02-22 22:15:53 +0900

    first commit

---
+++ src/main/java/net/iamchan/myhobbypage/MyhobbypageApplication.java
@@ -0,0 +1,13 @@
+package net.iamchan.myhobbypage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhobbypageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyhobbypageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/iamchan/myhobbypage/MyhobbypageApplicationTests.java
@@ -0,0 +1,13 @@
+package net.iamchan.myhobbypage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhobbypageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
